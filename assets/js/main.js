e35453ac5a2e62752d895668f44a06f1a09b07e7
Renuka Ghate <renuka@ajency.in>
2016-12-16 20:08:38 +0530

    working on the fly out footer

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$(document).on('click', '.rucard', function(e) {
+		$('.flyout').addClass('flyout--show');
+		$('.flyout__backdrop').addClass('flyout__backdrop--fadein');
+		$('.flyout__content').removeClass('flyout__content--hidden');
+	});
+	$(document).on('click', '.flyout__backdrop', function(e) {
+		$('.flyout').removeClass('flyout--show');
+		$('.flyout__backdrop').removeClass('flyout__backdrop--fadein');
+		$('.flyout__content').addClass('flyout__content--hidden');
+	});
+});
