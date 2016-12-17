78da2e441653f6bb7998c77c00047f8e1c092b11
jeromie <jeromie@ajency.in>
2016-12-17 12:39:54 +0530

    images added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,9 @@ $(window).on("load",function(){
     	theme:"minimal-dark",
     	scrollInertia: 200
     });
+	$( '#cd-dropdown' ).dropdown( {
+		gutter : 5,
+		stack : false,
+		slidingIn : 100
+	} );
 });
